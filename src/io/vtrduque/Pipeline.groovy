a8adcd134fbdf24539aa9819e76fea64aaec8937
Vitor Duque <vtrduque@gmail.com>
2018-01-23 10:02:31 -0200

    Add some functions

--- src/io/vtrduque/Pipeline.groovy
+++ src/io/vtrduque/Pipeline.groovy
@@ -5,3 +5,18 @@ package io.vtrduque;
 def helloWorld() {
   println "Hello world :)"
 }
+
+def npmInsstall() {
+  sh 'npm install'
+}
+
+def dockerBuildImage(image) {
+  sh "docker build -t ${imageName}  ."
+}
+
+def dockerPush(registry, credentials, imageName, prefix, version) {
+  docker.withRegistry(registry, credentials) {
+    fullName = "$prefix-$version"
+    docker.image(imageName).push(fullName)
+  }
+}
